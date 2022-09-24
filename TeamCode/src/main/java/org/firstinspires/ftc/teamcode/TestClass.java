48ac33d766abcb7a53c3f84f85b8d82b38b1a676
BotClub <black.leader.y.t01@gmail.com>
2022-09-24 13:14:31 -0400

    Made motor spin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * Use this as a template for making new opmodes.
+ */
+
+@TeleOp(name="TestClass")
+public class TestClass extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //INITIALIZATION CODE
+        DcMotor Motor1 = hardwareMap.get(DcMotor.class, "Motor1");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+        //RUN ONE TIME CODE HERE
+        Motor1.setPower(0.3);
+        Thread.sleep(1000);
+        Motor1.setPower(0);
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //LOOPING CODE HERE
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+            //test comment
+        }
+    }
+}
+
