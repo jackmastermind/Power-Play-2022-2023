234b78921101b3b0f88e9b805c0fd9561106f11c
jackmastermind <93287488+jthompson2023@users.noreply.github.com>
2022-09-24 12:52:15 -0400

    added template opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlankOpMode.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * Use this as a template for making new opmodes.
+ */
+
+@TeleOp(name="BlankOpMode")
+@Disabled
+public class BlankOpMode extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        //INITIALIZATION CODE
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+        //RUN ONE TIME CODE HERE
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //LOOPING CODE HERE
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        }
+    }
+}
+
