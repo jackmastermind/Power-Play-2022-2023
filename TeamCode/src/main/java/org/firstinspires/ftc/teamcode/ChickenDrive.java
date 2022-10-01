2ef9aa530de48a6428fd3fc218a9013b278e96a7
BotClub <black.leader.y.t01@gmail.com>
2022-10-01 16:46:23 -0400

    Added a program to control the chicken, tank-style Added an experimental program to control the chicken with one joystick

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChickenDrive.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+ * Use this as a template for making new opmodes.
+ */
+
+@TeleOp(name="ChickenDrive")
+public class ChickenDrive extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //INITIALIZATION CODE
+
+        DcMotor MotorLeft = hardwareMap.get(DcMotor.class, "driveLeft");
+        DcMotor MotorRight = hardwareMap.get(DcMotor.class, "driveRight");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+        // CHICKEN CODE BELOW HERE
+
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //LOOPING CODE HERE
+
+            MotorLeft.setPower(gamepad1.left_stick_y);
+            MotorRight.setPower(gamepad1.right_stick_y);
+
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Left Wheel Power", gamepad1.left_stick_y);
+            telemetry.addData("Right Wheel Power", gamepad1.right_stick_y);
+
+            telemetry.update();
+
+
+        }
+    }
+}
+
