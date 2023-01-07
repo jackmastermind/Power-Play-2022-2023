1fae26cab6587723d0b404bdf490ad1b4ced5729
jackmastermind <93287488+jthompson2023@users.noreply.github.com>
2023-01-07 10:32:45 -0500

    DiffMap_Master updated to include attachments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffMap_Master.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffMap_Master.java
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class DiffMap_Master extends HardwareMap_Master{
     public DiffSwerve diff = new DiffSwerve();
+
+    public DcMotor susan, slide;
+    public Servo claw, wrist;
 
     @Override
     public void init(HardwareMap ahwMap, boolean chassisOnly) {
@@ -12,7 +17,11 @@ public class DiffMap_Master extends HardwareMap_Master{
         motors = diff.motors;
         if (!chassisOnly)
         {
-            //TODO: Initialize attachments
+            susan = hwMap.get(DcMotor.class, "susan");
+            slide = hwMap.get(DcMotor.class, "spool");
+
+            claw = hwMap.get(Servo.class, "claw");
+            wrist = hwMap.get(Servo.class, "wrist");
         }
     }
 }
