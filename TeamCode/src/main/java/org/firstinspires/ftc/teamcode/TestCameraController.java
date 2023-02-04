9e45af539a91f261c7fdd87e8388fb1ee98d0ff0
jackmastermind <82781663+jackmastermind@users.noreply.github.com>
2023-02-04 10:40:00 -0500

    added TestCameraController

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCameraController.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Use this as a template for making new opmodes.
+ */
+
+@TeleOp(name="Test Camera Controller")
+public class TestCameraController extends LinearOpMode {
+
+    // Declare OpMode members.
+    private final ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        //INITIALIZATION CODE
+        CameraController cameraController = new CameraController(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+        //RUN ONE TIME CODE HERE
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //LOOPING CODE HERE
+            telemetry.addData("QR id", cameraController.GetQrCode());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+            //test comment
+        }
+    }
+}
+
